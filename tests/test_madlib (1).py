905c7ce3e280d10d146b4df7e5bda121130b6c46
Ian Cargill <idcargill@gmail.com>
2021-12-08 19:13:33 -0800

    remove duplicate

--- tests/test_madlib (1).py
+++ tests/test_madlib (1).py
@@ -1,5 +1,10 @@
 import pytest
 from madlib_cli.madlib import read_template, parse_template, merge
+from madlib_cli import __version__
+
+
+def test_version():
+    assert __version__ == '0.1.0'
 
 
 def test_read_template_returns_stripped_string():
