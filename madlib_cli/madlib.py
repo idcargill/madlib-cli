e4aee7c5d4b270054065e93a8931a5a68f5392a2
Ian Cargill <idcargill@gmail.com>
2021-12-11 12:01:43 -0800

    wip

--- madlib_cli/madlib.py
+++ madlib_cli/madlib.py
@@ -1,5 +1,6 @@
 import re
-from .welcome_message import welcome
+from Madlib_Module.welcome_message import welcome
+
 
 test_path = 'assets/dark_and_stormy_night_template.txt'
 sample_template = 'assets/sample_template.txt'
@@ -13,6 +14,7 @@ def parse_template(text):
   fill_in = r"{[\w ',.-]+}"
   prompts = re.findall(fill_in, text)
   text_template = re.sub(fill_in, '{}', text)
+  return prompts
   # print(prompts)
   # print(text_template.format('cat', 'hat', 'bat'))
   
@@ -23,6 +25,23 @@ def merge():
 
 text = read_template(test_path)
 
-parse_template(text)
+print(parse_template(text))
+
+print(text)
 
 
+def take_input(arr):
+  user_answers = []
+  for i in arr:
+    i = i.replace('{', '')
+    i = i.replace('}', '')
+    if i == 'Adjective':
+      user_input = input(f'Next word {i} -->')
+      user_answers.append(user_input)
+    user_input = input(f'Next word {i} -->')
+    user_answers.append(user_input)
+
+  return user_answers
+
+
+print(take_input(parse_template(text)))
